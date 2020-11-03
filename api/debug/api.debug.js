58e86aa042ea7bf52284d6ca415656bbf6456d46
vivianeflowt <vivianeflowt@gmail.com>
2020-11-03 18:18:50 -0300

    fix

---
+++ api/debug/api.debug.js
@@ -0,0 +1,40 @@
+// @
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+// const Authorization = require('../middlewares/authorization');
+
+// @ Controller
+
+const debug = async (req, res) => {
+  //
+  const { method } = req;
+
+  return res.status(200).json({ message: 'debug', method });
+};
+
+const deleteDatabase = async (req, res) => {
+  try {
+    await mongoose.connection.db.dropDatabase();
+    // throw new Error('aaa');
+  } catch (error) {
+    if (error) return res.status(400).send({ success: false, error });
+  }
+  return res.status(200).send({
+    success: true
+  });
+};
+
+// router.use(Authorization);
+// @ ROUTES
+// router.use(authorization);
+router.get('/', debug);
+router.post('/', debug);
+router.post('/', debug);
+router.put('/', debug);
+router.patch('/', debug);
+router.delete('/', deleteDatabase);
+
+module.exports = router;
--- api/index.js
+++ api/index.js
@@ -3,13 +3,10 @@
 const express = require('express');
 
 const router = express.Router();
-const mongoose = require('mongoose');
 const AccountRouter = require('./account/api.account');
 const AuthRouter = require('./auth/api.auth');
 const SecurityRouter = require('./security/api.security');
-
-// @ MAIN MIDDLEWARE
-// router.use(DebugMiddleware);
+const DebugRouter = require('./debug/api.debug');
 
 // @ ROOT ROUTER
 router.get('/', async (req, res) => {
@@ -21,44 +18,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-// // @ DEBUG ROUTER
-// router.post('/debug', async (req, res) => {
-//   try {
-//     const c = new Credencial({
-//       context: req.body.context || '',
-//       description: req.body.description || '',
-//       privileges: { key: 111, value: 122 } || ''
-//     });
-//     await c.save();
-//     return res.status(200).json({ success: true });
-//   } catch (error) {
-//     return res.status(400).json({ success: false, error });
-//   }
-// });
-
-// router.get('/debug', async (req, res) => {
-//   try {
-//     const allcred = await Credencial.find({}).exec();
-//     res.status(200).json(allcred);
-//   } catch (error) {
-//     res.status(400).send({ success: false, error });
-//   }
-// });
-
-router.delete('/debug', async (req, res) => {
-  try {
-    await mongoose.connection.db.dropDatabase();
-    // throw new Error('aaa');
-  } catch (error) {
-    if (error) return res.status(400).send({ success: false, error });
-  }
-  return res.status(200).send({
-    success: true
-  });
-});
-
 router.use('/api/account', AccountRouter);
 router.use('/api/auth', AuthRouter);
 router.use('/api/security', SecurityRouter);
+router.use('/api/debug', DebugRouter);
+router.use('/debug', DebugRouter);
 
 module.exports = router;
