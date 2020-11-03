60bdc28e5af5e3428cd53a75c3d207312a788574
vivianeflowt <vivianeflowt@gmail.com>
2020-11-03 16:50:22 -0300

    auth refactor

--- services/Authentication.Service.js
+++ services/Authentication.Service.js
@@ -1,43 +1,14 @@
 const Account = require('../database/models/Account');
 
-const VerifyByEmail = async (options = {}) => {
-  const { email, password } = options;
-  try {
-    const account = await Account.findOne({ email }).exec();
-    if (!account) return false;
-    return await account.verifyPassword(password);
-  } catch {
-    return false;
-  }
-};
-
-const VerifyByUserName = async (options = {}) => {
-  const { username, password } = options;
-  try {
-    const account = await Account.findOne({ username }).exec();
-    if (!account) return false;
-    return await account.verifyPassword(password);
-  } catch {
-    return false;
-  }
-};
-
 const Authenticate = async (options = {}) => {
   const { username, email, password } = options;
   console.log(options);
   try {
-    const message = {
-      success: null
-    };
-    if (email !== undefined) {
-      message.success = await VerifyByEmail({ email, password });
-    } else if (username !== undefined) {
-      message.success = await VerifyByUserName({ username, password });
-    } else {
-      message.success = false;
-    }
-    console.log(message);
-    return message;
+    const account = await Account.findOne({ $or: [{ email }, { username }] }).exec();
+    if (!account) return { success: false };
+    const isVerified = (await account.verifyPassword(password)) || false;
+
+    return { success: isVerified };
   } catch (error) {
     return { success: false, error };
   }
