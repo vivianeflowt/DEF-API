5dc31ed8558d13762aa9fc427428825d306651ce
vivianeflowt <vivianeflowt@gmail.com>
2020-11-04 08:35:37 -0300

    config

--- services/Authorization.Service.js
+++ services/Authorization.Service.js
@@ -4,6 +4,8 @@ const Account = require('../database/models/Account');
 
 const { config } = global;
 
+const privateKey = config.security.key.private;
+
 const SignIn = async (options = {}) => {
   const { username, email, password } = options;
   console.log(options);
@@ -15,7 +17,7 @@ const SignIn = async (options = {}) => {
       return { success: false };
     }
     const { id } = account;
-    const token = jwt.sign({ id }, config.security.key.private, {
+    const token = jwt.sign({ id }, privateKey, {
       expiresIn: '1h'
     });
     return { success: isVerified, token };
@@ -31,11 +33,11 @@ const Verify = async (options = {}) => {
     return { authorization: false, message: 'no token provided' };
   }
 
-  const decoded = await jwt.verify(accessToken, config.security.key.private);
+  const decoded = await jwt.verify(accessToken, privateKey);
   if (!decoded) {
     return { authorization: false, message: 'invalid token' };
   }
-  const refreshToken = await jwt.sign({ id: decoded.id }, config.security.key.private, {
+  const refreshToken = await jwt.sign({ id: decoded.id }, privateKey, {
     expiresIn: '1h'
   });
 
